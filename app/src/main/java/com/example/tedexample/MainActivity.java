dbdc84d641db19617db0e6304bd2d45da7720cad
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-11-13 08:57:15 +0630

    insert tab layout and change app used color

--- app/src/main/java/com/example/tedexample/MainActivity.java
+++ app/src/main/java/com/example/tedexample/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.tedexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tl_tablayout);
+        TabItem tabVideos = (TabItem) findViewById(R.id.tabVideo);
+        TabItem tabInfo = (TabItem) findViewById(R.id.tabAboutSpeaker);
+
+
     }
 }
